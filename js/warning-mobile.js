eac272456be2573c5900fff78748894a5f708994
IgorRoc <igor_roc@hotmail.com.br>
2021-06-13 18:52:30 -0300

    [add]: Mobile warning

---
+++ js/warning-mobile.js
@@ -0,0 +1,8 @@
+
+function continueMobile() {
+	let mobile = document.getElementById("mobile")
+	let everything = document.getElementById("everything")
+
+	mobile.style.display = "none"
+	everything.style.display = "block"
+}
